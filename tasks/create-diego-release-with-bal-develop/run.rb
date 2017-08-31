e341d64e861b5f6d9e6ae784d49b6871c0c53f62
Dave Goddard <dave@goddard.id.au>
2017-08-31 11:03:43 -0400

    wip

--- tasks/create-diego-release-with-bal-develop/run.rb
+++ tasks/create-diego-release-with-bal-develop/run.rb
@@ -4,6 +4,7 @@
 require 'fileutils'
 require 'yaml'
 
+version = "0.#{Time.now.to_i}"
 bal_develop_sha = nil
 
 Dir.chdir 'bal-develop' do
@@ -14,7 +15,13 @@ Dir.chdir 'diego-release' do
   Dir.chdir 'src/code.cloudfoundry.org/buildpackapplifecycle' do
     system(%(git checkout "#{bal_develop_sha}"))
   end
-  system(%(bosh --parallel 10 sync blobs && bosh create release --force --with-tarball --name diego --version 0.#{Time.now.to_i})) || raise('cannot create diego-release')
+  system(%(bosh --parallel 10 sync blobs && bosh create release --force --with-tarball --name diego --version #{version})) || raise('cannot create diego-release')
 end
 
 system('rsync -a diego-release/ diego-release-artifacts') || raise('cannot rsync directories')
+
+File.write("diego-release-artifacts/use-diego-dev-release.yml", "---
+- path: /releases/name=diego
+  type: replace
+  value:
+    version: #{version}")
