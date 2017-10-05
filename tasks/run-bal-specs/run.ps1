e282424e12d796859f76fe1e0470e7e7a1693a72
Sam Coward <scoward@pivotal.io>
2017-10-05 12:26:23 -0400

    wip
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- tasks/run-bal-specs/run.ps1
+++ tasks/run-bal-specs/run.ps1
@@ -3,7 +3,7 @@ trap { $host.SetShouldExit(1) }
 
 $env:CREDENTIAL_FILTER_WHITELIST="SystemDrive,SERVICE_ID,NUMBER_OF_PROCESSORS,PROCESSOR_LEVEL,WINSW_SERVICE_ID,__PIPE_SERVICE_NAME"
 
-$env:GOPATH="C:/go" + Get-Random
+$env:GOPATH="C:/go-" + (-join ((48..57) + (97..122) | Get-Random -Count 6 | % {[char]$_}))
 $env:PATH=$env:GOPATH + "/bin;C:/go/bin;C:/var/vcap/bosh/bin;" + $env:PATH
 
 $buildDir=$env:GOPATH + "/src/code.cloudfoundry.org/buildpackapplifecycle"
