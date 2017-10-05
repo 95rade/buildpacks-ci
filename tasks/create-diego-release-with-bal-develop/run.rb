d68b7736ccafee1c459d5fcfc98f67fddb89f041
Sam Coward <scoward@pivotal.io>
2017-10-05 14:20:10 -0400

    wip
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- tasks/create-diego-release-with-bal-develop/run.rb
+++ tasks/create-diego-release-with-bal-develop/run.rb
@@ -15,9 +15,15 @@ Dir.chdir 'diego-release' do
   Dir.chdir 'src/code.cloudfoundry.org/buildpackapplifecycle' do
     system(%(git fetch && git checkout "#{bal_develop_sha}")) || raise('could not update buildpackapplifecycle in diego-release')
   end
-  Dir.chdir 'src/github.com/cloudfoundry-incubator/credhub-cli' do
-    system(%(git remote add idoru https://github.com/idoru/credhub-cli && git fetch && git checkout idoru/interpolate-api)) || raise('could not get idoru fork of credhub-cli')
+
+  # Dir.chdir 'src/github.com/cloudfoundry-incubator/credhub-cli' do
+  #   system(%(git remote add idoru https://github.com/idoru/credhub-cli && git fetch && git checkout idoru/interpolate-api)) || raise('could not get idoru fork of credhub-cli')
+  # end
+  FileUtils.mkdir_p 'src/github.com/cloudfoundry-incubator'
+  Dir.chdir 'src/github.com/cloudfoundry-incubator' do
+    system(%(git clone https://github.com/idoru/credhub-cli && git checkout interpolate-api)) || raise('could not get idoru fork of credhub-cli')
   end
+
   system(%(bosh --parallel 10 sync blobs && bosh create release --force --with-tarball --name diego --version #{version})) || raise('cannot create diego-release')
 end
 
