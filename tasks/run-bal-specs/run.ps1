9d2af8330e1a07653004056e11b2abdab6c29ce4
Sam Coward <scoward@pivotal.io>
2017-10-11 15:11:36 -0400

    wip; clean up GOPATH in trap
    Signed-off-by: Victoria Henry <vhenry@pivotal.io>

--- tasks/run-bal-specs/run.ps1
+++ tasks/run-bal-specs/run.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = "Stop";
 $env:GOPATH="C:\go-" + (-join ((48..57) + (97..122) | Get-Random -Count 6 | % {[char]$_}))
-trap { $host.SetShouldExit(1) }
+trap { $host.SetShouldExit(1); cmd.exe /c rd /q /s $env:GOPATH }
 
 $env:CREDENTIAL_FILTER_WHITELIST="SystemDrive,SERVICE_ID,NUMBER_OF_PROCESSORS,PROCESSOR_LEVEL,WINSW_SERVICE_ID,__PIPE_SERVICE_NAME"
 
