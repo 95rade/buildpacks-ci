a74be7c488b66cf3e86de72d18f14f15a002f1b7
Sam Coward <scoward@pivotal.io>
2017-10-18 01:51:34 -0400

    shouldnt be necessary to do this anymore on diego-release#develop

--- tasks/create-diego-release-with-bal-develop/run.rb
+++ tasks/create-diego-release-with-bal-develop/run.rb
@@ -16,18 +16,6 @@ Dir.chdir 'diego-release' do
     system(%(git fetch && git checkout "#{bal_develop_sha}")) || raise('could not update buildpackapplifecycle in diego-release')
   end
 
-  FileUtils.mkdir_p 'src/github.com/cloudfoundry-incubator'
-  Dir.chdir 'src/github.com/cloudfoundry-incubator' do
-    system(%(git clone https://github.com/cloudfoundry-incubator/credhub-cli.git))
-  end
-
-  %w(buildpack_app_lifecycle windows_app_lifecycle).each do |name|
-    spec = YAML.load_file("packages/#{name}/spec")
-    spec['files'] << 'code.cloudfoundry.org/buildpackapplifecycle/*/*.go'
-    spec['files'] << 'github.com/cloudfoundry-incubator/credhub-cli/**/*.go'
-    File.write("packages/#{name}/spec", spec.to_yaml)
-  end
-
   system(%(bosh --parallel 10 sync blobs && bosh create release --force --with-tarball --name diego --version #{version})) || raise('cannot create diego-release')
 end
 
