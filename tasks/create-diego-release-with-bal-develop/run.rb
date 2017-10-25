f07aa1f823b87d5f1a7bcd0c576d894f643e6258
Sam Coward <scoward@pivotal.io>
2017-10-25 11:55:13 -0400

    set GOPATH before runnign sync-package-specs; yet anoyther WIP

--- tasks/create-diego-release-with-bal-develop/run.rb
+++ tasks/create-diego-release-with-bal-develop/run.rb
@@ -16,7 +16,7 @@ Dir.chdir 'diego-release' do
     system(%(git fetch && git checkout "#{bal_develop_sha}")) || raise('could not update buildpackapplifecycle in diego-release')
   end
 
-  system(%(git status && ./scripts/sync-package-specs && git status && git diff && bosh --parallel 10 sync blobs && bosh create release --force --with-tarball --name diego --version #{version})) || raise('cannot create diego-release')
+  system(%(export GOPATH=$PWD && ./scripts/sync-package-specs && git status && git diff && bosh --parallel 10 sync blobs && bosh create release --force --with-tarball --name diego --version #{version})) || raise('cannot create diego-release')
 end
 
 system('rsync -a diego-release/ diego-release-artifacts') || raise('cannot rsync directories')
