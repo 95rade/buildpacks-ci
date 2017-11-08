967c12e935525def724cde488bf93c7e034688bf
Jack Feeny <jfeeny@pivotal.io>
2017-11-08 15:50:27 -0500

    use github releases
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/create-bump-buildpacks-opsfile/run.rb
+++ tasks/create-bump-buildpacks-opsfile/run.rb
@@ -4,20 +4,25 @@
 require 'fileutils'
 require 'yaml'
 
+version = "0.#{Time.now.to_i}"
+
 replacements = []
-Dir.glob("*-buildpack-bosh-io-release").each do |bosh_release|
-  release_name = bosh_release.gsub("-bosh-io-release", "")
+Dir.glob("*-buildpack-github-release").each do |bosh_release|
+  release_name = bosh_release.gsub("-github-release", "")
 
   replacements << {
     path: "/releases/name=#{release_name}"
     type: "replace"
     value: {
       name: release_name,
-      version: File.read("#{bosh_release}/version").strip,
-      sha1: File.read("#{bosh_release}/sha1").strip,
-      url: File.read("#{bosh_release}/url").strip
+      version: version
     }
   }
+
+  Dir.chdir(bosh_release) do
+    system(%(bosh --parallel 10 sync blobs && bosh create release --force --with-tarball --name #{release_name} --version #{version})) || raise("cannot create #{release_name} release")
+    system(%(cp dev_releases/*/*.tgz ../built-buildpacks-artifacts/))
+  end
 end
 
 replacements << {
@@ -31,5 +36,5 @@ replacements << {
     }
 }
 
-File.open("bump-buildpacks-opsfile/opsfile.yml", 'w') {|f| f.write replacements.to_yaml }#
+File.open("bump-buildpacks-opsfile/opsfile.yml", 'w') {|f| f.write replacements.to_yaml }
 
