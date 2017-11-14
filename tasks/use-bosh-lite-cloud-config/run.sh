ff960ff06cf03fd1127cf5f0c56a09c5bb2a6580
Victoria Henry <vhenry@pivotal.io>
2017-11-14 10:51:41 -0500

    wip; add some debugging
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/use-bosh-lite-cloud-config/run.sh
+++ tasks/use-bosh-lite-cloud-config/run.sh
@@ -3,9 +3,11 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -o xtrace
 
 pushd bbl-state/"${ENV_NAME}"
   eval "$(bbl print-env)"
+  bosh2 env
 popd
 
 bosh2 update-cloud-config cf-deployment/iaas-support/bosh-lite/cloud-config.yml
