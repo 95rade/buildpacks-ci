4eb9f53be6d0cdba0bf7d3957019e3ab1d2dee72
Dave Goddard <dave@goddard.id.au>
2018-02-16 14:00:03 -0500

    wip
    Signed-off-by: Leah Hanson <lhanson@pivotal.io>

--- tasks/delete-deployment/run.sh
+++ tasks/delete-deployment/run.sh
@@ -4,6 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+wget https://github.com/cloudfoundry/bosh-bootloader/releases/download/v5.11.5/bbl-v5.11.5_linux_x86-64
+chmod 755 bbl-v5.11.5_linux_x86-64
+alias bbl "$PWD/bbl-v5.11.5_linux_x86-64"
+
 pushd "bbl-state/$ENV_NAME"
   eval "$(bbl print-env)"
   bosh2 -n delete-deployment -d "${DEPLOYMENT_NAME}"
