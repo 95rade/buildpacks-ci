6e8658587e252460ed0119dcac95de8a29261f7e
Leah Hanson <lhanson@pivotal.io>
2018-02-16 14:01:07 -0500

    wip
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- tasks/delete-deployment/run.sh
+++ tasks/delete-deployment/run.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 wget https://github.com/cloudfoundry/bosh-bootloader/releases/download/v5.11.5/bbl-v5.11.5_linux_x86-64
 chmod 755 bbl-v5.11.5_linux_x86-64
-alias bbl "$PWD/bbl-v5.11.5_linux_x86-64"
+alias bbl="$PWD/bbl-v5.11.5_linux_x86-64"
 
 pushd "bbl-state/$ENV_NAME"
   eval "$(bbl print-env)"
